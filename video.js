fa6ecb9a5c0440508d6e618c612b8f4de8165d33
Usshy0229 <38417160+Usshy0229@users.noreply.github.com>
2018-04-28 15:27:52 +0900

    Update video.js

--- video.js
+++ video.js
@@ -1,6 +1,6 @@
 var w = window, player;
 // 再生したいyoutubeの動画id
-var vid = 'SlRfwbD2riE';
+var vid = 'OBVFJZMXOkA;
 
 var tag = document.createElement('script');
 tag.src = "https://www.youtube.com/iframe_api";
